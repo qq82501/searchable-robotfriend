64e593ce9f76cc3b54007930aea69bd40753fd63
qq82501 <qq82501@gmail.com>
2022-04-19 14:10:23 +0800

    Updates

---
+++ precache-manifest.ba53dfc0cefdf90e10110822bf86f3ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f372ba194514f250773",
+    "url": "/static/css/main.208590b6.chunk.css"
+  },
+  {
+    "revision": "2f372ba194514f250773",
+    "url": "/static/js/main.2f372ba1.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "20c6ea2347cba2bc4041",
+    "url": "/static/css/2.e25b1ed9.chunk.css"
+  },
+  {
+    "revision": "20c6ea2347cba2bc4041",
+    "url": "/static/js/2.20c6ea23.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "169c09160a7da2334832c602144c983c",
+    "url": "/static/media/1up.169c0916.ttf"
+  },
+  {
+    "revision": "146d45a0da268ae15fa6901743d9a9d7",
+    "url": "/index.html"
+  }
+];
